385c1bf66894086dd01e6259764f5700f97e2e0c
aakashdeveloper <ahanda205@gmail.com>
2021-01-03 09:53:23 +0530

    a

--- day6/7storage.js
+++ day6/7storage.js
@@ -26,3 +26,12 @@ localStorage.setItem('abc','433462436')
 sessionStorage.setItem('token','4355453')
 sessionStorage.getItem('token')
 sessionStorage.removeItem('token')
+
+document.cookie
+"_ga=GA1.2.1880173027.1602255983; origin=delhi; type=resume"
+document.cookie="city=london"
+"city=london"
+document.cookie
+"_ga=GA1.2.1880173027.1602255983; origin=delhi; type=resume; _gid=GA1.2.2040491829.1609647605; city=london"
+document.cookie="country=england;expires=Mon, 04 Jan 2021 01:00:00 UTC"
+"country=england;expires=Mon, 04 Jan 2021 01:00:00 UTC"
